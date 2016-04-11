85d006d61da5bf829cfd053d2afb05bcf42a07a4
Fernando <fcoelho@hugeinc.com>
2016-04-11 12:55:28 -0300

    Add deploy files

--- src/gulpfile.babel.js
+++ src/gulpfile.babel.js
@@ -21,27 +21,27 @@ import ghPages from 'gulp-gh-pages';
 import rollupConfig from './rollup.config';
 
 const srcPaths = {
-    js: 'src/js/main.js',
-    css: 'src/styl/**/*.styl',
-    mainStyl: 'src/styl/main.styl',
-    jade: 'src/templates/**/*.jade',
-    img: 'src/img/**/*'
+  js: 'src/js/main.js',
+  css: 'src/styl/**/*.styl',
+  mainStyl: 'src/styl/main.styl',
+  jade: 'src/templates/**/*.jade',
+  img: 'src/img/**/*',
 };
 
 const buildPaths = {
-    build: 'build/**/*',
-    js: 'build/js/',
-    css: 'build/css/',
-    jade: 'build/',
-    img: 'build/img'
+  build: 'build/**/*',
+  js: 'build/js/',
+  css: 'build/css/',
+  jade: 'build/',
+  img: 'build/img',
 };
 
 gulp.task('css', () => {
-    gulp.src(srcPaths.mainStyl)
-        .pipe(sourcemaps.init())
+  gulp.src(srcPaths.mainStyl)
+      .pipe(sourcemaps.init())
         .pipe(stylus({
-            use: [koutoSwiss(), prefixer(), jeet(), rupture()],
-            compress: true
+          use: [koutoSwiss(), prefixer(), jeet(), rupture()],
+          compress: true,
         }))
         .pipe(gcmq())
         .pipe(cssnano())
@@ -50,54 +50,54 @@ gulp.task('css', () => {
 });
 
 gulp.task('js', () => {
-    gulp.src(srcPaths.js)
-        .pipe(plumber())
-        .pipe(rollup(rollupConfig))
-        .pipe(uglify())
-        .pipe(gulp.dest(buildPaths.js));
+  gulp.src(srcPaths.js)
+      .pipe(plumber())
+      .pipe(rollup(rollupConfig))
+      .pipe(uglify())
+      .pipe(gulp.dest(buildPaths.js));
 });
 
 gulp.task('jade', () => {
-    gulp.src(srcPaths.jade)
-        .pipe(plumber())
-        .pipe(jade())
-        .pipe(gulp.dest(buildPaths.jade));
+  gulp.src(srcPaths.jade)
+      .pipe(plumber())
+      .pipe(jade())
+      .pipe(gulp.dest(buildPaths.jade));
 });
 
 gulp.task('images', () => {
-    gulp.src(srcPaths.img)
-        .pipe(plumber())
+  gulp.src(srcPaths.img)
+      .pipe(plumber())
         .pipe(imagemin({
-            optimizationLevel: 3,
-            progressive: true,
-            interlaced: true
+          optimizationLevel: 3,
+          progressive: true,
+          interlaced: true,
         }))
         .pipe(gulp.dest(buildPaths.img));
 });
 
 gulp.task('watch', () => {
-    gulp.watch(srcPaths.jade, ['jade']);
-    gulp.watch(srcPaths.css, ['css']);
-    gulp.watch(srcPaths.js, ['js']);
-    gulp.watch(srcPaths.img, ['images']);
+  gulp.watch(srcPaths.jade, ['jade']);
+  gulp.watch(srcPaths.css, ['css']);
+  gulp.watch(srcPaths.js, ['js']);
+  gulp.watch(srcPaths.img, ['images']);
 });
 
 gulp.task('browser-sync', () => {
-    var files = [
-        buildPaths.build
-    ];
+  var files = [
+      buildPaths.build,
+  ];
 
-    browserSync.init(files, {
-        server: {
-            baseDir: './build/'
-        },
-    });
+  browserSync.init(files, {
+    server: {
+      baseDir: './build/',
+    },
+  });
 });
 
 gulp.task('pages', () => {
-    gulp.src(buildPaths.build)
-        .pipe(ghPages());
+  gulp.src(buildPaths.build)
+      .pipe(ghPages());
 });
 
 gulp.task('default', ['css', 'jade', 'js', 'images', 'watch', 'browser-sync']);
-gulp.task('deploy', ['css', 'jade', 'js', 'images', 'pages']);
+gulp.task('deploy', ['css', 'jade', 'js', 'images']);
