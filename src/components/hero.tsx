f3198b488d741eb9f741f813a596b2622e688a2c
Fernando Coelho <nandoacoelho@gmail.com>
2025-02-23 00:17:30 +0100

    feat: hide wip links

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Hero: React.FC = () => {
   return (
-    <article className="hero p-4 md:p-8 w-full 2xl:max-w-[70%]">
+    <article className="hero flex justify-center align-center p-4 md:p-8 w-full 2xl:max-w-[70%]">
       <p className="message font-serif font-light text-4xl md:text-8xl/24 md:tracking-tight">
         <span className="name italic">Fernando Coelho</span>, is a Frontend
         engineer based in Barcelona currently connecting people through form
