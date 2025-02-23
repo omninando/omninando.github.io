afe5f50d4b7739f74869bbdbea47d0bb327739d4
Fernando Coelho <nandoacoelho@gmail.com>
2025-02-23 23:51:28 +0100

    feat: update footer on mobile

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Footer: React.FC = () => {
   return (
     <footer className="footer flex justify-center w-full p-4 pb-12 md:p-0 md:pb-12 md:pt-30 font-medium text-sm md:text-base">
-      <div className="wrapper w-full md:max-w-[90%] flex flex-col md:flex-row gap-10 justify-between items-center">
+      <div className="wrapper w-full md:max-w-[90%] flex flex-wrap items-start flex-row gap-10 justify-between items-center">
         <p>© Fernando Coelho 2025</p>
-        <div className="social-links flex flex-wrap flex-center justify-center md:justify-end w-[60%] md:w-auto gap-4">
+        <div className="social-links flex flex-wrap flex-center justify-start md:justify-end w-full md:w-auto gap-6 md:gap-4">
           <a
             className="link"
             href="mailto:nandoacoelho@gmail.com"
