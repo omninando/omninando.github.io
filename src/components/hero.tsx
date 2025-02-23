9bd6cd25b76c0714f9bc6c0327b0c15654d99e98
Fernando Coelho <nandoacoelho@gmail.com>
2025-02-23 23:46:05 +0100

    feat: update letter spacing on mobile

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -15,7 +15,7 @@ const Hero: React.FC = () => {
   return (
     <section className="hero flex justify-center align-center w-full h-full p-4 md:p-0">
       <div className="hero-content w-full md:max-w-[90%] h-[100vh] flex flex-col justify-center">
-        <p className="message md:max-w-[80%] font-serif font-light text-4xl md:text-6xl md:tracking-tight">
+        <p className="message md:max-w-[80%] font-serif font-light text-4xl md:text-6xl tracking-[-2.1px] md:tracking-tight">
           Olá, I'm{' '}
           <span className="name italic font-medium">Fernando Coelho</span>, a
           Brazilian Frontend Engineer passionate about crafting meaningful
